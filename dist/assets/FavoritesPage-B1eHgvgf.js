9091462612997ef3070dc338cbe145bc69610ace
kristipapallazo <papallazokristi@gmail.com>
2025-04-16 21:15:09 +0200

    .

--- dist/assets/FavoritesPage-B1eHgvgf.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BCaJyBng.js";const s="_favorites_page_13e84_1",a={favorites_page:s},o=()=>e.jsx("div",{className:a.favorites_page,children:"FavoritesPage"});export{o as default};
--- dist/assets/MyWebsitePage-gtUpThmm.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BCaJyBng.js";const s=()=>e.jsx("div",{children:"MyWebsitePage"});export{s as default};
